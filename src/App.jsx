9fb86a3fdb9993048a295162ce748cb8b761829e
Amsterdan Vasconcelos <amsterdan.sci@gmail.com>
2024-08-05 19:57:59 -0300

    feat: test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 const App = () => {
   const login = async () => {
-    const result = await fetch(
-      'https://booking-room-backend.vercel.app/api/users/login',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: 'jessica@email.com',
-          password: '123456',
-        }),
-      }
-    );
+    const result = await fetch('/api/users/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: 'jessica@email.com',
+        password: '123456',
+      }),
+    });
 
     console.log(await result.json());
   };
